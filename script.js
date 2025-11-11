40288af48e3e3c52fd76db3365b2810f21b9d70c
N'GUESSAN <128142008+debady@users.noreply.github.com>
2025-11-11 09:20:21 +0000

    Update download link to TikeTa_3.apk

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const bouton = document.getElementById("btnTelecharger");
 const animationZone = document.getElementById("animationZone");
 
 // URL du fichier APK ou du store
-const lienTelechargement = "statique/TikeTa_2.apk"; // à remplacer
+const lienTelechargement = "statique/TikeTa_3.apk"; // à remplacer
 
 bouton.addEventListener("click", () => {
   // 1️⃣ Téléchargement de l'application
